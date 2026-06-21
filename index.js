659429989d8bcf2670d3f55f34aae66c5375e2c3
arsen <arseniy0311@gmail.com>
2026-06-21 13:17:19 +0200

    Day 2, rewrote in express, figured out public folder with css / js in it

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
-const fs = require("fs")
-const http = require("http")
+const express = require("express")
+const path = require("path")
 
-const server = http.createServer((req,res)=> {
-  if (req.url == "/"){
-    fs.readFile('./page.html', 'utf-8',(err, data)=>{
-      if (err){
-        console.log(err)
-        res.end('error reading file')
-        return // off switch so the code does not run further
-      } 
-      res.writeHead(200, {"Content-Type": "text/html"})
-      res.end(data)
-    })
-  } else if (req.url == "/about"){
-    res.end('abtpage')
-  }
+const app = express()
+app.use(express.static("public"))
+app.get("/", (req, res)=>{
+  res.sendFile(path.join(__dirname, "/page.html"))
+})
+app.get("/about", (req, res)=>{
+  res.send("abt page!")
 })
 
-server.listen('3000')
+app.listen(3000, () => {console.log("app is running on 3000")})
+
+
+// const fs = require("fs")
+// const http = require("http")
+// const server = http.createServer((req,res)=> {
+//   if (req.url == "/"){
+//     fs.readFile('./page.html', 'utf-8',(err, data)=>{
+//       if (err){
+//         console.log(err)
+//         res.end('error reading file')
+//         return // off switch so the code does not run further
+//       } 
+//       res.writeHead(200, {"Content-Type": "text/html"})
+//       res.end(data)
+//     })
+//   } else if (req.url == "/about"){
+//     res.end('abtpage')
+//   }
+// })
+
+// server.listen('3000')
---
+++ public/app.js
@@ -0,0 +1,6 @@
+console.log("client js is loaded")
+
+const reviewButton = document.getElementById("review-button")
+reviewButton.addEventListener('click', (e) => {
+  console.log('review sent!')
+})
