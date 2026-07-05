cfcc4dc01df8c6312591016482f9a2061f86d5b4
arsen <arseniy0311@gmail.com>
2026-07-05 12:17:26 +0200

    day 4.5, reuilt from scratch, forgot a bit how to do post request

--- index.js
+++ index.js
@@ -2,57 +2,26 @@ const express = require("express")
 const path = require("path")
 
 const app = express()
+
 app.use(express.static("public"))
 app.use(express.json())
 
-
-app.get("/", (req, res)=>{
+app.get("/", (req,res)=>{
   res.sendFile(path.join(__dirname, "/page.html"))
 })
-app.get("/about", (req, res)=>{
-  res.send("abt page!")
-})
-app.get("/time", (req, res) => {
-  setTimeout(() => {
+app.get("/time", (req,res)=>{
+  setTimeout(()=>{
     res.send(new Date().toString())
-  }, 5000)
+  }, 1000)  
 })
 app.get("/hello", (req,res)=>{
-  res.send("Enter a username in url to see the profile")
+  res.send("Enter username in url to see profile")
 })
-app.get("/hello/:name", (req, res)=> {
-  console.log(req.params)
+app.get("/hello/:name", (req,res)=>{
   res.send(`Hello, ${req.params.name}`)
 })
-app.get("/hello/:first/:last", (req,res)=>{
-  console.log(req.params)
-  res.send(`${req.params.first, req.params.last}`)
-})
-app.get("/json", (req,res)=>{
-  res.json({message:"hello", day:5})
-})
-app.post("/data", (req,res)=>{  
+app.post("/data/", (req,res)=>{
   console.log(req.body)
+  res.json({received: req.body})
 })
-app.listen(3000, () => {console.log("app is running on 3000")})
-
-
-// const fs = require("fs")
-// const http = require("http")
-// const server = http.createServer((req,res)=> {
-//   if (req.url == "/"){
-//     fs.readFile('./page.html', 'utf-8',(err, data)=>{
-//       if (err){
-//         console.log(err)
-//         res.end('error reading file')
-//         return // off switch so the code does not run further
-//       } 
-//       res.writeHead(200, {"Content-Type": "text/html"})
-//       res.end(data)
-//     })
-//   } else if (req.url == "/about"){
-//     res.end('abtpage')
-//   }
-// })
-
-// server.listen('3000')
+app.listen(3000, ()=>{console.log(`App runs on: ${"http://localhost:3000/"}`)})
---
+++ index_done.js
@@ -0,0 +1,58 @@
+const express = require("express")
+const path = require("path")
+
+const app = express()
+app.use(express.static("public"))
+app.use(express.json())
+
+
+app.get("/", (req, res)=>{
+  res.sendFile(path.join(__dirname, "/page.html"))
+})
+app.get("/about", (req, res)=>{
+  res.send("abt page!")
+})
+app.get("/time", (req, res) => {
+  setTimeout(() => {
+    res.send(new Date().toString())
+  }, 5000)
+})
+app.get("/hello", (req,res)=>{
+  res.send("Enter a username in url to see the profile")
+})
+app.get("/hello/:name", (req, res)=> {
+  console.log(req.params)
+  res.send(`Hello, ${req.params.name}`)
+})
+app.get("/hello/:first/:last", (req,res)=>{
+  console.log(req.params)
+  res.send(`${req.params.first, req.params.last}`)
+})
+app.get("/json", (req,res)=>{
+  res.json({message:"hello", day:5})
+})
+app.post("/data", (req,res)=>{  
+  console.log(req.body)
+})
+app.listen(3000, () => {console.log("app is running on 3000")})
+
+
+// const fs = require("fs")
+// const http = require("http")
+// const server = http.createServer((req,res)=> {
+//   if (req.url == "/"){
+//     fs.readFile('./page.html', 'utf-8',(err, data)=>{
+//       if (err){
+//         console.log(err)
+//         res.end('error reading file')
+//         return // off switch so the code does not run further
+//       } 
+//       res.writeHead(200, {"Content-Type": "text/html"})
+//       res.end(data)
+//     })
+//   } else if (req.url == "/about"){
+//     res.end('abtpage')
+//   }
+// })
+
+// server.listen('3000')
